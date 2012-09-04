7f688e743cbd9f16a9ceee1877d637f3b659a4f0
Vlad.Iliescu.88@gmail.com <Vlad.Iliescu.88@gmail.com@42f960a1-aad4-b08e-6b72-5efd33d37d32>
2012-09-04 09:30:43 +0000

    initial commit

---
+++ util/global_options.py
@@ -0,0 +1,50 @@
+__author__ = 'Vlad'
+
+from storage import Storage
+from functools import wraps
+from cStringIO import StringIO
+
+def _pull_nodes(stage):
+    temp_nodes = Kinetic.Global.temp_nodes
+    for key in temp_nodes:
+        node = temp_nodes[key]
+        if node.get_stage() is not None and id(node.get_stage()) == id(stage):
+            stage._add_id(node)
+            stage._add_name(node)
+            Kinetic.Global._remove_temp_node(node)
+
+Kinetic = Storage()
+Kinetic.Filters = Storage()
+Kinetic.Plugins = Storage()
+Kinetic.Global = Storage({
+    'BUBBLE_WHITELIST': ['mousedown', 'mousemove', 'mouseup', 'mouseover', 'mouseout', 'click', 'dblclick',
+                       'touchstart', 'touchmove', 'touchend', 'tap', 'dbltap', 'dragstart', 'dragmove', 'dragend'],
+    'BUFFER_WHITELIST': ['fill', 'stroke', 'text_fill', 'text_stroke'],
+    'BUFFER_BLACKLIST': ['shadow'],
+    'id_counter': 0,
+    'temp_nodes': Storage(),
+    '_add_temp_node': lambda node : Kinetic.Global.temp_nodes.update({id(node): node}),
+    '_remove_temp_node' : lambda node : Kinetic.Global.temp_nodes.update({id(node): None}),
+    '_pull_nodes': _pull_nodes,
+    'shapes': Storage(),
+    'filename': StringIO(),
+    'tab': 1
+})
+
+def write_output(fn, file=Kinetic.Global.filename):
+    @wraps(fn)
+    def wrapper(*args, **kw):
+        result = fn(*args, **kw)
+        file.write('\t'*Kinetic.Global.tab+result+"\n")
+        return result
+    return wrapper
+
+def write_to_file(file):
+    file = open(file, 'wb')
+    io = Kinetic.Global.filename
+    io.seek(0)
+    file.write('window.onload = function() {\n')
+    file.write(io.read())
+    file.write('};')
+    file.close()
+
---
+++ util/type.py
@@ -0,0 +1,60 @@
+__author__ = 'Vlad'
+
+from storage import Storage
+from random import randint
+
+class Type():
+
+    @classmethod
+    def is_number(cls, number):
+        return isinstance(number, (int, float, long))
+
+    @classmethod
+    def pep_to_js(cls, col):
+        cols = {
+            'rotation_deg': 'rotationDeg',
+            'drag_constraint': 'dragConstraint',
+            'drag_bounds': 'dragBounds',
+            'stroke_width': 'strokeWidth',
+            'line_join': 'lineJoin',
+            'draw_func': 'drawFunc',
+            'clear_before_draw': 'clearBeforeDraw',
+            'corner_radius': 'cornerRadius'
+        }
+        if col in cols:
+            return cols[col]
+        return col
+
+    @classmethod
+    def format(cls, val):
+        if isinstance(val, basestring):
+            return '"%s"' %val
+        elif isinstance(val, bool):
+            return '%s' %('true' if val else 'false')
+        elif isinstance(val, (dict, Storage)):
+            _s = []
+            for i in val:
+                _s.append('%s: %s' %(Type.pep_to_js(i), Type.format(val[i])))
+            return '{%s}' %', '.join(_s)
+        elif Type.is_number(val):
+            return  '%r' %val
+        elif isinstance(val, (list, tuple)):
+            _s = []
+            for i in val:
+                _s.append('%s' %Type.format(i))
+            return '[%s]' % ', '.join(_s)
+        elif val is None:
+            return 'undefined'
+
+
+    @classmethod
+    def rgb_to_hex(cls, r, g, b):
+        return hex((1 << 24) + (r << 16) + (g << 8) + b)[3:]
+
+    @classmethod
+    def get_random_color_key(cls):
+        r = randint(1, 255)
+        g = randint(1, 255)
+        b = randint(1, 255)
+        return cls.rgb_to_hex(r, g, b)
+
